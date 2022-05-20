5956de42190cfa46ab6ffc0ff480d10ea21e64f4
skool <zacharski@gmail.com>
2022-05-19 19:15:35 -0500

    fix put/get

--- src/main/java/com/mcneilio/shokuyoku/Worker.java
+++ src/main/java/com/mcneilio/shokuyoku/Worker.java
@@ -93,7 +93,7 @@ public class Worker {
                     }
                 }
 
-                EventDriver eventDriver = drivers.put(eventName+date, null);
+                EventDriver eventDriver = drivers.get(eventName+date);
                 if (eventDriver!=null) {
                     eventDriver.addMessage(msg);
                 }
